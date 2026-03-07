b08b4a0beb972f4a87af28be25d8a534ea05242b
jbekhazi <jbekhazi@smith.edu>
2026-03-07 12:53:15 -0500

    draft 1

--- ShopTest.java
+++ ShopTest.java
@@ -1,8 +1,119 @@
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.Test.None;
+
+import java.util.ArrayList;
+// what it is that you are checking for, we need to find the wrong thing
+// we need to identify 10 bugs
 
 public class ShopTest {
+    Computer comp;
+    ResaleShop shop;
 
+
+    @Before
+    public void setup(){
+        this.comp = new Computer("2017 MacBook Pro", "Intel", 256, 16, "High Sierra", 2017, 800);
+        this.shop = new ResaleShop();
+    }
+
+    private static final int TIMEOUT = 2000;
     // add your tests here 
+
+    @Test(timeout = TIMEOUT)
+    public void testSetPrice(){
+        comp.setPrice(700);
+        assertEquals(700, comp.price);
+    }
     
+    @Test(timeout = TIMEOUT)
+    public void testSetOS(){
+        comp.setOS("new OS");
+        assertEquals("new OS", comp.operatingSystem);
+    }
+    // first bug, the method setOS sets the OS to none instead of the new Os input. 
+
+    @Test(timeout = TIMEOUT)
+    public void testGetYear(){
+        assertEquals(2017, comp.yearMade);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testBuy(){
+        shop.buy(comp);
+        int inventorySize = shop.inventory.size();
+        assertEquals(comp, shop.inventory.get(inventorySize - 1));   
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testDoubleBuy(){
+        shop.buy(comp);
+        int sizeBefore = shop.inventory.size();
+        shop.buy(comp);
+        int sizeAfter = shop.inventory.size();
+        assertEquals(sizeBefore, sizeAfter); 
+    }  
+
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testSell(){
+        shop.sell(comp);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSellAnother(){
+        shop.sell(comp);
+        // ?? idk which one, we could also use this with the buy function perhaps 
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testRefurbish2000(){
+        comp.yearMade = 1999;
+        shop.inventory.add(comp);
+        shop.refurbish(comp, "None");
+        assertEquals(0, comp.price);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testRefurbish2012(){
+        comp.yearMade = 2011;
+        shop.inventory.add(comp);
+        shop.refurbish(comp, "None");
+        assertEquals(250, comp.price);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testRefurbish2018(){
+        comp.yearMade = 2017;
+        shop.inventory.add(comp);
+        shop.refurbish(comp, "None");
+        assertEquals(550, comp.price);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testRefurbishElse(){
+        comp.yearMade = 2019;
+        shop.inventory.add(comp);
+        shop.refurbish(comp, "None");
+        assertEquals(1000, comp.price);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testRefurbishNewOs(){
+        shop.inventory.add(comp);
+        shop.refurbish(comp, "new OS");
+        assertEquals("new OS", comp.operatingSystem);
+    }  
+    
+    @Test(expected = RuntimeException.class)
+    public void testRefurbishException(){
+        shop.refurbish(comp, "new OS");
+        // checks if the computer is not in the inventory 
+        // but the problem is that it keeps running aftewards and then breaks??? we don't want that 
+        //But IDK what kind of expection do we need with that 
+    }
+
 }
+
--- bugLog.md
+++ bugLog.md
@@ -1,26 +1,28 @@
 ## Bug 1
-Brief description:  
-Failed unit test: 
+Brief description:  the method setOS sets the OS to none instead of updating it to the new Os input. 
+
+Failed unit test: testSetOS
 
 ## Bug 2
-Brief description:  
-Failed unit test: 
+Brief description:  the buy function overwrites the computer we want to add to the inventory with a 2019 Macbook computer. We do not end up adding our own computer. 
+Failed unit test: testBuy
 
 ## Bug 3
-Brief description:  
-Failed unit test: 
+Brief description: The Buy function buys a computer without checking first if it already exists in the inventory. 
+Failed unit test: testDoubleBuy
 
 ## Bug 4
-Brief description:  
-Failed unit test: 
+Brief description:  The function can not handle a computer not in its inventory being sold. 
+Failed unit test: testSell
 
 ## Bug 5
-Brief description:  
-Failed unit test: 
+Brief description: A computer older than 2012 (but newer than 2000) should have a price of 250$, but refurbish gives it a price of 2500$  
+Failed unit test: testRefurbish2012
 
 ## Bug 6
-Brief description:  
-Failed unit test: 
+Brief description:  This function fails because the setOs function also fails and we debugged it earlier. 
+HOWEVER, the resale shop method should ask for an optional string and NOT a string "None" so that is another error that I do not know how to test for? How do I test for parameters being wrong?
+Failed unit test: testRefurbishSetOS
 
 ## Bug 7
 Brief description:  
